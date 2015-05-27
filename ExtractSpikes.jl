ef5e6db0bd1201b5f85a7146ac3553f1d8c8e1c6
paulmthompson <pmt10@case.edu>
2015-05-27 17:19:07 -0400

    added MTEO method

--- ExtractSpikes.jl
+++ ExtractSpikes.jl
@@ -3,6 +3,7 @@ module ExtractSpikes
 
 using DSP
 
+#This runs first and calls detect spikes
 function extractSpikes
 
     #Set default params
@@ -15,6 +16,9 @@ function extractSpikes
 
 end
 
+function detectSpikes
+
+end
 
 
 function runningAverage(rawSignal, windowSize, mode=1)
@@ -29,5 +33,36 @@ function runningAverage(rawSignal, windowSize, mode=1)
                     
 end
 
+function MTEO(rawSignal, ks)
+
+    tmp=zeros(Float64,length(ks),lenght(rawSignal))
+
+    for i=1:length(ks)
+        tmp[i,:] = runningTEO(rawSignal, ks[i])
+        v = var(tmp[i,:])
+
+        #apply the window
+        win = hamming( 4*ks(i)+1, 'symmetric')
+        tmp[i,:] = filter( win,1,tmp[i,:]) ./ v
+
+    end
+
+    return sum(tmp)
+
+end
+
+
+
+function runningTEO(rawSignal,k=1)
+
+    out=similar(rawSignal)
+    for i=1:length(rawSignal)
+        #out = rawSignal[i]^2 - [rawSignal(1+k:end) repmat(0,1,k)].*[repmat(0,1,k) rawSignal(1:end-k)]
+        out[i] = rawSignal[i]^2 - rawSignal[i-1]*rawSignal[i+1]
+    end
+    
+    return out
+    
+end
 
 end

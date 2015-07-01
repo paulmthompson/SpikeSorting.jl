aabfef7ceec3ddf5a501f16e0f94854ca85cd63e
paulmthompson <pmt10@case.edu>
2015-06-30 20:01:35 -0400

    added method to clear graph after it is filled

--- SortSpikes.jl
+++ SortSpikes.jl
@@ -1,8 +1,8 @@
 module SortSpikes
 
-using ExtractSpikes
+using ExtractSpikes, Winston, Gtk.ShortNames
 
-export onlineCal, onlineSort, offlineSort, onlineCal_par
+export onlineCal, onlineSort, offlineSort, onlineCal_par, get_context, plotline
 
 #Should define types here
 
@@ -74,6 +74,41 @@ end
 function offlineSort()
 
     
+end
+
+function get_context(c::Gtk.Canvas, pc::Winston.PlotContainer)
+    device = Winston.CairoRenderer(Gtk.cairo_surface(c))
+    ext_bbox = Winston.boundingbox(device)
+    Winston._get_context(device, ext_bbox, pc)
+end
+
+function make2(self::Winston.Curve, context::Winston.PlotContext)
+    x, y = Winston.project(context.geom, self.x, self.y)
+    y = context.draw.ctx.surface.height - y
+    Winston.GroupPainter(Winston.getattr(self,:style), Winston.PathPainter(x,y))
+end
+
+function plotline(i::Int64,c::Array{Gtk.Canvas,1},p::Array{Any,1},cu::Array{Any,2},a::Winston.PlotContext)
+
+    d=make2(cu[i,1],a)
+
+    Winston.paint(d,a.paintc)
+
+    reveal(c[i],false)
+    
+end
+
+function wipeout(context::Winston.PlotContext)
+    N = 50
+    imgdata = reshape([ 0x00ffffff for i in 0:(N^2-1) ], (N, N))
+    img = Winston.Image((1, N), (1, N), round(UInt32,imgdata))
+    
+    a = Winston.project(context.geom, Winston.Point(img.x, img.y))
+    b = Winston.project(context.geom, Winston.Point(img.x+img.w, img.y+img.h))
+    bbox = Winston.BoundingBox(a, b)
+    bbox2=Winston.BoundingBox(bbox.xmin,bbox.xmax,
+    context.draw.ctx.surface.height - bbox.ymax,context.draw.ctx.surface.height - bbox.ymin)
+    Winston.GroupPainter(Winston.getattr(img,:style), Winston.ImagePainter(img.img, bbox2))
 end
 
 end

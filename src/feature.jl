6581cbe78d66f3265aa7ec5616cda1c6ec43a267
Paul Thompson <pmt10@case.edu>
2015-07-19 19:13:36 -0400

    added integral transform feature

--- src/feature.jl
+++ src/feature.jl
@@ -7,7 +7,7 @@ Feature extraction methods. Each method needs
 
 =#
 
-export FeatureTime, FeaturePCA
+export FeatureTime, FeaturePCA, FeatureIT
 
 function featureprepare{D<:Detect,C<:Cluster,A<:Align,F<:Feature}(sort::Sorting{D,C,A,F})
     nothing
@@ -70,3 +70,111 @@ end
 function mysize(feature::FeatureWPD,wavelength::Int64)
     feature.oPCA.k
 end
+
+#=
+Integral Transform
+
+Zviagintsev et al 2006
+=#
+
+type FeatureIT <: Feature
+    N1::Int64
+    N2::Int64
+    wavemean::Float64
+    itr::Int64
+    tbeta::Int64
+    talpha::Int64
+end
+
+function FeatureIT()
+    FeatureIT(0,0,0.0,0,0,0)
+end
+
+function feature{D<:Detect,C<:Cluster,A<:Align,F<:FeatureIT}(sort::Sorting{D,C,A,F})
+    sort.features[:]=zeros(Float64,length(sort.features))
+    for i=sort.f.talpha:(sort.f.talpha+sort.f.N1)
+        sort.features[1]+=sort.waveforms[i,sort.numSpikes]
+    end
+    sort.features[1]=sort.features[1]/sort.f.N1
+
+    for i=sort.f.tbeta:(sort.f.tbeta+sort.f.N2)
+        sort.features[2]+=sort.waveforms[i,sort.numSpikes]
+    end
+    sort.features[2]=sort.features[2]/sort.f.N2
+    
+    nothing
+end
+
+function mysize(feature::FeatureIT,wavelength::Int64)
+    2
+end
+
+function featureprepare{D<:Detect,C<:Cluster,A<:Align,F<:FeatureIT}(sort::Sorting{D,C,A,F})
+
+    sort.f.wavemean=mean(sort.waveforms[:,sort.numSpikes])
+    tempN1=0
+    tempN2=0
+    thisval=0
+    lastval=0
+    N1=0
+    N2=0
+    talpha=0
+    talphat=0
+    tbeta=0
+    tbetat=0
+    
+    for i=1:size(sort.waveforms,1)
+
+        thisval=sign(sort.waveforms[i,sort.numSpikes]-sort.f.wavemean)
+        if thisval==lastval
+            if thisval==1
+                tempN2+=1
+                if tempN2>N2
+                    N2=tempN2
+                    tbeta=tbetat
+                end
+            else
+                tempN1+=1
+                if tempN1>N1
+                    N1=tempN1
+                    talpha=talphat
+                end
+            end  
+        else
+            if thisval==1
+                tempN2=1
+                tbetat=i
+            else
+                tempN1=1
+                talphat=i
+            end            
+        end
+        lastval=thisval
+    end
+
+    if sort.f.itr==0
+        sort.f.N1=N1
+        sort.f.N2=N2
+        sort.f.talpha=talpha
+        sort.f.tbeta=tbeta
+    else
+        sort.f.N1=round((sort.f.itr/(sort.f.itr+1))*sort.f.N1+(1/(sort.f.itr+1))*N1)
+        sort.f.N2=round((sort.f.itr/(sort.f.itr+1))*sort.f.N2+(1/(sort.f.itr+1))*N2)
+        sort.f.talpha=round((sort.f.itr/(sort.f.itr+1))*sort.f.talpha+(1/(sort.f.itr+1))*talpha)
+        sort.f.tbeta=round((sort.f.itr/(sort.f.itr+1))*sort.f.tbeta+(1/(sort.f.itr+1))*tbeta)
+    end
+
+    if sort.f.N1+sort.f.talpha>size(sort.waveforms,1)
+        sort.f.N1=size(sort.waveforms,1)-sort.f.talpha-1
+    end
+    if sort.f.N2+sort.f.tbeta>size(sort.waveforms,1)
+        sort.f.N2=size(sort.waveforms,1)-sort.f.tbeta-1
+    end
+    
+
+    sort.f.itr+=1
+    
+    nothing
+    
+end
+
